eff50597b8b070172f559441c7c02fab17fab893
Alicia Godefroy <aliciagodefroy24@gmail.com>
2022-05-18 19:35:36 +0200

    final

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -59,13 +59,13 @@ exports.deleteSauce = (req, res, next) => {
       // console.log('data.userId', data.userId)
       // console.log('decodedUserId', decodedUserId)
       Sauce.findOne({ _id: req.params.id })
-    .then(sauce => {
-      const filename = sauce.imageUrl.split('/images/')[1];
-      fs.unlink(`images/${filename}`, () => {
-        Sauce.deleteOne({ _id: req.params.id })
-          .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
-          .catch(error => res.status(400).json({ error }));
-      });
+      .then(sauce => {
+        const filename = sauce.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+          Sauce.deleteOne({ _id: req.params.id })
+            .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
+            .catch(error => res.status(400).json({ error }));
+        });
     })
     .catch(error => res.status(500).json({ error }));
     } 
